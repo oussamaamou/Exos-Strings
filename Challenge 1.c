fd0c70b8f8fa3b16483745ec4bfe2f6f3a892137
oussamaamou <amoussdark@gmail.com>
2024-09-08 09:14:58 +0000

    Challenge 1.c

---
+++ Challenge 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+    int Chaine[10];
+    printf("Veuillez entrer une chaine de caractere :");
+    scanf("%s",Chaine);
+    printf("%s",Chaine);
+
+
+return 0;
+}
